f968849b3449e8d6f7188566a5ace8e6ad022dd5
Lakhwinder Singh <76493064+yuvi-420@users.noreply.github.com>
2021-05-02 20:06:17 -0700

    photo gallery app

---
+++ src/views/Tab2.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery App</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+    <ion-fab-button @click="takePhoto()">
+      <ion-icon :icon="camera"></ion-icon>
+    </ion-fab-button>
+  </ion-fab>
+      
+      
+     
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { camera, trash, close } from 'ionicons/icons';
+import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, 
+         IonToolbar, IonTitle, IonContent,  } from '@ionic/vue';
+import { usePhotoGallery} from '@/composables/usePhotoGallery';
+
+
+export default  {
+  name: 'Tab2',
+  components: { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, 
+         IonToolbar, IonTitle, IonContent,  },
+  setup() {
+     const { takePhoto } = usePhotoGallery();
+
+    return {
+      camera, trash, close,takePhoto
+    }
+  }
+}
+</script>
